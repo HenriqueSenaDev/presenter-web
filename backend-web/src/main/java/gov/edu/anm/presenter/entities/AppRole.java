b8cb8b55c5e1a85c3ccd467fd26ecaa16df2a099
HenriqueSenaDev <luizteste656@gmail.com>
2023-01-15 00:13:50 -0300

    back:AppRole Enum/UserDetails Impl

--- backend-web/src/main/java/gov/edu/anm/presenter/entities/AppRole.java
+++ backend-web/src/main/java/gov/edu/anm/presenter/entities/AppRole.java
@@ -1,25 +1,5 @@
 package gov.edu.anm.presenter.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Table(name = "APP_ROLES")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class AppRole {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
-
+public enum AppRole {
+    ADMIN
 }
--- backend-web/src/main/java/gov/edu/anm/presenter/entities/AppUser.java
+++ backend-web/src/main/java/gov/edu/anm/presenter/entities/AppUser.java
@@ -1,26 +1,26 @@
 package gov.edu.anm.presenter.entities;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collection;
+import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
 @Table(name = "APP_USERS")
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AppUser {
+public class AppUser implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,7 +28,33 @@ public class AppUser {
     private String username;
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    Set<AppRole> roles = new HashSet<>();
+    @Enumerated(EnumType.STRING)
+    private AppRole role;
 
+    // UserDetails username/password gettters/setters will be set up by lombok because they have same name in AppUser
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
