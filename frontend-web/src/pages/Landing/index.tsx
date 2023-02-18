88a4254fc5d301a39b608e89293e082d60a5367e
HenriqueSenaDev <luizteste656@gmail.com>
2023-02-18 14:43:52 -0300

    front:Landing-page-fix

--- frontend-web/src/pages/Landing/index.tsx
+++ frontend-web/src/pages/Landing/index.tsx
@@ -1,14 +1,13 @@
-import Navbar from "components/Navbar";
-import "./styles.css";
-import { Context } from "context/AppContextProvider";
-
 import { ReactComponent as PeopleIcon } from "assets/images/people-Icon.svg";
 import { ReactComponent as TimerIcon } from "assets/images/timer-Icon.svg";
 import { ReactComponent as ComputerIcon } from "assets/images/computer-Icon.svg";
 import { ReactComponent as RankingIcon } from "assets/images/leaderboard-Icon.svg";
 import { useState, useContext } from "react";
 import { Navigate } from "react-router-dom";
+import { Context } from "context/AppContextProvider";
+import Navbar from "components/Navbar";
 import Loading from "pages/Loading";
+import "./styles.css";
 
 const Landing = () => {
    const [username, setUsername] = useState<string>("");
@@ -17,31 +16,28 @@ const Landing = () => {
 
    const { handleLogin, authenticated } = useContext(Context);
 
-   async function login() {
+   async function signIn() {
       setLoading(true);
-      await handleLogin(username, password);
+      await handleLogin(username.trim(), password.trim());
       setLoading(false);
    }
 
-   if (authenticated) {
-      return <Navigate replace to="/library" />
-   }
+   if (authenticated) return <Navigate replace to="/library" />
 
-   if (loading) {
-      return (
-         <Loading />
-      );
-   }
+   if (loading) return <Loading />;
 
    return (
       <>
          <div className="landing--wrapper">
             <Navbar />
+
             <div className="landing--content">
                <div className="landing--card">
                   <div className="landing--card--content">
                      <h1>Presenter</h1>
+
                      <p>Gerenciamento de competições baseadas em equipes com jurados e tabelas de ranking.</p>
+
                      <div className="landing--content--images">
                         <PeopleIcon />
                         <TimerIcon />
@@ -49,37 +45,32 @@ const Landing = () => {
                         <RankingIcon />
                      </div>
                   </div>
-                  <hr></hr>
+
+                  <hr />
+
                   <div className="landing--card--form">
                      <h1>Entrar</h1>
+
                      <input
                         type="text"
                         placeholder="Username"
                         value={username}
                         minLength={5}
-                        onChange={(event) => {
-                           setUsername(event.target.value);
-                        }}
-                     ></input>
+                        onChange={(evt) => setUsername(evt.target.value)}
+                     />
+
                      <input
                         type="password"
                         placeholder="Password"
                         value={password}
                         minLength={5}
-                        onChange={(event) => {
-                           setPassword(event.target.value);
-                        }}
+                        onChange={(evt) => setPassword(evt.target.value)}
                         onKeyUp={(event) => {
-                           if (event.key === "Enter") {
-                              login();
-                           }
+                           if (event.key === "Enter") signIn();
                         }}
-                     ></input>
-                     <button
-                        onClick={() => {
-                           login();
-                        }}
-                     >
+                     />
+
+                     <button onClick={() => signIn()}>
                         Login
                      </button>
                   </div>
