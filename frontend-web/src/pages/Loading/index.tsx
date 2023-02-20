6dc1bee7b4fb5408c3ea6d3245b872793ed0e412
HenriqueSenaDev <luizteste656@gmail.com>
2023-02-20 17:03:38 -0300

    front:loadingGif-fix/strict-mode-off

--- frontend-web/src/pages/Loading/index.tsx
+++ frontend-web/src/pages/Loading/index.tsx
@@ -1,12 +1,12 @@
+import loadingGif from "assets/images/loading.gif";
 import './styles.css';
-import WhiteGif from "assets/images/white-Gif.gif";
 
 const Loading = () => {
    return (
       <div className="loading--page">
          <h1>Presenter</h1>
 
-         <img src={WhiteGif} alt="loading-gif" ></img>
+         <img src={loadingGif} alt="loading-gif" ></img>
       </div>
    );
 }
