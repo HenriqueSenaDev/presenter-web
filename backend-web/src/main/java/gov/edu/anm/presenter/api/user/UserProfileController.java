cd298d6bf4bebb5887903d722783a74d045e9b37
HenriqueSenaDev <luizteste656@gmail.com>
2023-03-06 21:06:37 -0300

    back:UserProfileController

---
+++ backend-web/src/main/java/gov/edu/anm/presenter/api/user/UserProfileController.java
@@ -0,0 +1,35 @@
+package gov.edu.anm.presenter.api.user;
+
+import gov.edu.anm.presenter.api.common.dtos.appuser.AppUserOutputDto;
+import gov.edu.anm.presenter.api.common.dtos.participation.UserParticipationOutputDto;
+import gov.edu.anm.presenter.api.common.utils.ProfileUtilities;
+import gov.edu.anm.presenter.domain.appuser.AppUser;
+import gov.edu.anm.presenter.domain.appuser.AppUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/user/profile")
+@RequiredArgsConstructor
+public class UserProfileController {
+    private final ProfileUtilities profileUtilities;
+    private final AppUserService appUserService;
+
+    @GetMapping
+    public ResponseEntity<AppUserOutputDto> getProfile() {
+        var user = (AppUser) profileUtilities.getAuthenticatedUser();
+        return ResponseEntity.ok(new AppUserOutputDto(user));
+    }
+
+    @GetMapping("/participations")
+    public ResponseEntity<List<UserParticipationOutputDto>> getUserParticipations() {
+        var user = (AppUser) profileUtilities.getAuthenticatedUser();
+        return ResponseEntity.ok(appUserService.findUserParticipations(user.getId()));
+    }
+
+}
