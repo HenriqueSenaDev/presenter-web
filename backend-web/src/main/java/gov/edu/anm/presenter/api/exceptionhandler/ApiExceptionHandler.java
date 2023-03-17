19e0127ec51d09b3d20b9b12d6c843f31d64451d
HenriqueSenaDev <luizteste656@gmail.com>
2023-03-17 13:17:55 -0300

    back:validation-MethodArgumentNotValidException-handling

--- backend-web/src/main/java/gov/edu/anm/presenter/api/exceptionhandler/ApiExceptionHandler.java
+++ backend-web/src/main/java/gov/edu/anm/presenter/api/exceptionhandler/ApiExceptionHandler.java
@@ -5,9 +5,14 @@ import gov.edu.anm.presenter.domain.exceptions.UnauthorizedRoleException;
 import gov.edu.anm.presenter.domain.exceptions.UnavailableSubjectException;
 import gov.edu.anm.presenter.domain.exceptions.UnmatchedCodeException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -19,6 +24,20 @@ import java.time.OffsetDateTime;
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+    private final MessageSource messageSource;
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ObjectError error = ex.getBindingResult().getAllErrors().get(0);
+        String field = ((FieldError) error).getField();
+        String fieldMessage = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+        String message = field + " " + fieldMessage;
+
+        var errorMessage = new ResponseErrorMessage(status.value(), OffsetDateTime.now(), message);
+
+        return handleExceptionInternal(ex, errorMessage, new HttpHeaders(), status, request);
+    }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
