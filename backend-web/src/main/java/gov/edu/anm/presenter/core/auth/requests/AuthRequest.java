2e767591b5d4119635ff346a1f41f2b6e57adb59
HenriqueSenaDev <luizteste656@gmail.com>
2023-03-17 13:44:29 -0300

    back:AuthController-validation

--- backend-web/src/main/java/gov/edu/anm/presenter/core/auth/requests/AuthRequest.java
+++ backend-web/src/main/java/gov/edu/anm/presenter/core/auth/requests/AuthRequest.java
@@ -5,11 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthRequest {
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
 }
