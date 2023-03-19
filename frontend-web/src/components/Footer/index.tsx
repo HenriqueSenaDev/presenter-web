daa4836cefc41faa85661476aaebaf81b8adf37f
HenriqueSenaDev <luizteste656@gmail.com>
2023-03-19 13:40:27 -0300

    front:Footer

---
+++ frontend-web/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function Footer() {
+    return (
+        <div className="footer-container">
+            <span>© 2023 HenriqueSenaDev. Todos os Direitos Reservados.</span>
+        </div>
+    );
+}
+
+export default Footer;
