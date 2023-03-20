6a26fba449bcfafd275abe795d2f267cf7483619
HenriqueSenaDev <luizteste656@gmail.com>
2023-03-19 21:50:42 -0300

    front:LoginPopUp

---
+++ frontend-web/src/pages/Landing/components/LoginPopUp/index.tsx
@@ -0,0 +1,56 @@
+import { useContext, useState } from "react";
+import { ProfileContext } from "context/ProfileContext";
+import Button from "components/Button";
+import FormField from "components/FormField";
+import loginImg from "../../../../assets/images/login.svg";
+import loadingGif from "../../../../assets/images/loading.gif";
+import "./styles.css";
+
+interface IProps {
+    setIsPopupOpen: Function
+}
+
+function LoginPopUp({ setIsPopupOpen }: IProps) {
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const { handleLogin } = useContext(ProfileContext);
+
+    async function signIn() {
+        setIsLoading(true);
+        await handleLogin(username.trim(), password.trim());
+        setIsLoading(false);
+    }
+
+    function checkOutClick({ target }: React.MouseEvent<HTMLElement>) {
+        if (document.querySelector('.login-popup-wrapper') === target) {
+            setIsPopupOpen(false);
+        }
+    }
+
+    return (
+        <div
+            className="login-popup-wrapper"
+            onClick={checkOutClick}
+        >
+            <div className="login-popup-card">
+                {isLoading && <img className='loading-gif' src={loadingGif} alt="loading-spiral" />}
+
+                <img className='login-img' src={loginImg} alt="girl-looking-to-form" />
+
+                <form className="login-form">
+                    <div className="login-form-fields-area">
+                        <FormField label="Usuário" setState={setUsername} />
+
+                        <FormField label="Senha" setState={setPassword} />
+                    </div>
+
+                    <Button text="Login" onClick={signIn} />
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default LoginPopUp;
--- frontend-web/src/pages/Landing/index.tsx
+++ frontend-web/src/pages/Landing/index.tsx
@@ -1,46 +1,40 @@
 import { useState, useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { ProfileContext } from "context/ProfileContext";
-import Loading from "pages/Loading";
 import Footer from "components/Footer";
 import DualButton from "components/DualButton";
+import LoginPopUp from "./components/LoginPopUp";
 import peopleImg from "../../assets/images/people.svg";
 import managementImg from "../../assets/images/management.svg";
 import "./styles.css";
 
 const Landing = () => {
-   const [username, setUsername] = useState<string>("");
-   const [password, setPassword] = useState<string>("");
-   const [loading, setLoading] = useState<boolean>(false);
+   const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
 
-   const { handleLogin, authenticated } = useContext(ProfileContext);
-
-   async function signIn() {
-      setLoading(true);
-      await handleLogin(username.trim(), password.trim());
-      setLoading(false);
-   }
+   const { authenticated } = useContext(ProfileContext);
 
    if (authenticated) return <Navigate replace to="/library" />
 
-   if (loading) return <Loading />;
-
    return (
       <div className="landing-container">
+         {isPopupOpen && <LoginPopUp setIsPopupOpen={setIsPopupOpen} />}
+
          <div className="landing-card-container">
             <div className="landing-card">
                <div className="landing-card-header-container">
                   <div className="landing-card-header">
                      <h1>Presenter</h1>
 
-                     <p>Gerenciamento de competições baseadas em equipes com jurados e tabelas de ranking.</p>
+                     <p>
+                        Gerenciamento de competições baseadas em equipes com jurados e tabelas de ranking.
+                     </p>
                   </div>
 
                   <img src={peopleImg} alt="people-image" />
                </div>
 
                <div className="landing-card-cta-container">
-                  <DualButton text='Entrar' onClick={() => { }} />
+                  <DualButton text='Entrar' onClick={() => setIsPopupOpen(true)} />
 
                   <img className="management-image" src={managementImg} alt="people-managing" />
                </div>
