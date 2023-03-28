6cdb1a623d28dead7d6af7d730bcbb4eed6c4339
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2023-03-28 20:43:39 -0300

    front:Menu

---
+++ frontend-web/src/components/Menu/index.tsx
@@ -0,0 +1,54 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { ProfileContext } from "context/ProfileContext";
+import profileIcon from "../../assets/images/profile.svg";
+import closeIcon from "../../assets/images/close.svg";
+import signOutIcon from "../../assets/images/sign-out.svg";
+import "./styles.css";
+
+interface IProps {
+    setIsMenuOpen: Function
+}
+
+function Menu({ setIsMenuOpen }: IProps) {
+    const { user, handleLogout } = useContext(ProfileContext);
+    const navigate = useNavigate();
+
+    return (
+        <aside className="menu-wrapper">
+            <div className="menu-container">
+                <div className="menu-header">
+                    <div className="profile-area">
+                        <img src={profileIcon} alt="profile-icon" />
+
+                        <span>{user!.username}</span>
+                    </div>
+
+                    <img 
+                        className="menu-close-img" 
+                        onClick={() => setIsMenuOpen(false)}
+                        src={closeIcon} 
+                        alt="x" 
+                    />
+                </div>
+
+                <div className="nav-area">
+                    <span onClick={() => navigate('/library')}>
+                        Biblioteca
+                    </span>
+                </div>
+
+                <div 
+                    className="log-out-area"
+                    onClick={() => handleLogout()}
+                >
+                    <img src={signOutIcon} alt="sign-out-icon" />
+
+                    <span>Sair</span>
+                </div>
+            </div>
+        </aside>
+    );
+}
+
+export default Menu;
