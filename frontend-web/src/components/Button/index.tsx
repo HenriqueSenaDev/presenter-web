31ab66775a2a0a91cf06a12552b678bc16987d85
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2023-03-29 11:06:18 -0300

    front:Button-cancel-background/css-components-fix

--- frontend-web/src/components/Button/index.tsx
+++ frontend-web/src/components/Button/index.tsx
@@ -3,14 +3,16 @@ import "./styles.css";
 
 interface IProps {
     text: string,
+    type?: 'confirm' | 'cancel',
     onClick: MouseEventHandler<HTMLDivElement>
 }
 
-function Button({ text, onClick }: IProps) {
+function Button({ text, type, onClick }: IProps) {
     return (
         <div 
             className="button"
             onClick={onClick}
+            style={type === 'cancel' ? { backgroundColor: '#76AFDA' } : {}}
         >
             {text}
         </div>
