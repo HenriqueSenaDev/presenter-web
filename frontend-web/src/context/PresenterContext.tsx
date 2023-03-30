78bce8a988fae832f4a4a3a2470e20d53253d7a6
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2023-03-30 19:48:13 -0300

    front:PresenterContext-fix

--- frontend-web/src/context/PresenterContext.tsx
+++ frontend-web/src/context/PresenterContext.tsx
@@ -1,7 +1,6 @@
+import { createContext, useState} from "react"
 import { IEvent, IParticipation } from "common/@types/presenter.types";
-import { createContext, useState, useContext } from "react"
 import { usePresenter } from "hooks/usePresenter";
-import { ProfileContext } from "context/ProfileContext";
 
 interface IPresenterContext {
     participations: IParticipation[], handleParticipations: Function,
@@ -14,11 +13,10 @@ function PresenterContextProvider({ children }: { children: React.ReactNode }) {
     const [participations, setParticipations] = useState<IParticipation[]>([]);
     const [event, setEvent] = useState<IEvent | null>(null);
 
-    const { user } = useContext(ProfileContext);
     const { findUserParticipations, findEvent } = usePresenter();
 
     async function handleParticipations() {
-        setParticipations(await findUserParticipations(user!.id));
+        setParticipations(await findUserParticipations());
     }
 
     async function handleEvent(eventId: number) {
