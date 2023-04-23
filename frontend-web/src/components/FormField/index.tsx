5e127ce637fe65d102a015a3bb0d2502ffc3198c
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2023-04-23 11:09:04 -0300

    front:FormField-toogle-password-visibility

--- frontend-web/src/components/FormField/index.tsx
+++ frontend-web/src/components/FormField/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import eyeIcon from '../../assets/images/eye.svg';
+import eyeSlashIcon from '../../assets/images/eye-slash.svg';
 import "./styles.css";
 
 interface IProps {
@@ -7,22 +10,44 @@ interface IProps {
     onEnter?: Function
 }
 
-function FormField({ label, type, setState, onEnter }: IProps) {
+function FormField({ label, type: propsType, setState, onEnter }: IProps) {
+    const [inputType, setInputType] = useState<'text' | 'password'>(propsType);
+
+    function getEyePassImgSource() {
+        if (inputType === 'password') return eyeIcon;
+        else return eyeSlashIcon;
+    }
+
+    function tooglePasswordVisibility() {
+        if (inputType === 'password') setInputType('text');
+        else setInputType('password');
+    }
+
     return (
         <div className="form-field-container">
             <div>
                 <span>{label}</span>
             </div>
 
-            <input 
-                type={type} 
-                name={label} 
-                id={label} 
-                onChange={(e) => setState(e.target.value)}
-                onKeyUp={(e) => {
-                    if (e.code === 'Enter' && onEnter) onEnter();
-                }}
-            />
+            <div className="form-input-area">
+                <input 
+                    type={inputType} 
+                    name={label} 
+                    id={label} 
+                    onChange={(e) => setState(e.target.value)}
+                    onKeyUp={(e) => {
+                        if (e.code === 'Enter' && onEnter) onEnter();
+                    }}
+                />
+
+                {(propsType === 'password') &&
+                    <img 
+                        src={getEyePassImgSource()} 
+                        alt="password-eye"
+                        onClick={tooglePasswordVisibility} 
+                    />
+                }
+            </div>
         </div>
     );
 }
