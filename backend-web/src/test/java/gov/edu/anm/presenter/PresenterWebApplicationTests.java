9c7e526531bfdc5e097c0e77186765522dc8aa3d
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2024-01-22 15:01:38 -0300

    back: tests hotfix

--- backend-web/src/test/java/gov/edu/anm/presenter/PresenterWebApplicationTests.java
+++ backend-web/src/test/java/gov/edu/anm/presenter/PresenterWebApplicationTests.java
@@ -1,13 +1,8 @@
 package gov.edu.anm.presenter;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PresenterWebApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
